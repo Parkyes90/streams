2984afed478c83182e1077a71ee1bcb174771052
parkyes90 <parkyes90@gmail.com>
2019-04-01 19:07:58 +0900

    add apps

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,7 +8,7 @@ class Header extends Component {
     return (
       <div className="ui secondary pointing menu">
         <Link to="/" className="item">
-          Streamyasd
+          Streamy
         </Link>
         <div className="right menu">
         <Link to="/" className="item">
--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,13 +1,44 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import {editStream, fetchStream} from "../../actions";
+import StreamForm from "./StreamForm";
+import _ from 'lodash';
 
 class StreamEdit extends Component {
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    this.props.fetchStream(id);
+  }
+
+  onSubmit = formValues => {
+    const { match: { params: { id } } } = this.props;
+    this.props.editStream(id, formValues);
+  };
+
   render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
     return (
-      <div>
-        StreamEdit
+       <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
+          onSubmit={this.onSubmit}
+        />
       </div>
     );
   }
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  return {stream: state.streams[ownProps.match.params.id]};
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchStream, editStream }
+)
+(StreamEdit);
+
